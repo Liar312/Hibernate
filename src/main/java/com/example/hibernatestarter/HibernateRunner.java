6f9ef1abec861bebcc21b6d1c72a6c449f7a12a2
Liar123 <krasn20161@mail.ru>
2024-07-27 02:45:39 +0300

    4 lesson

---
+++ src/main/java/com/example/hibernatestarter/HibernateRunner.java
@@ -0,0 +1,9 @@
+package com.example.hibernatestarter;
+
+import java.sql.DriverManager;
+
+public class HibernateRunner {
+    public static void main(String[] args) {
+        DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",postgres,Nthvbynjh2015)
+    }
+}
---
+++ src/test/java/com/example/hibernatestarter/HibernateStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hibernatestarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
