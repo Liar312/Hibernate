8ea2576d8376e723227e157e4ea90ba6618b677c
Liar123 <krasn20161@mail.ru>
2024-07-28 01:00:24 +0300

    полностью настроил конфиг и session factory

--- src/main/java/com/example/hibernatestarter/HibernateRunner.java
+++ src/main/java/com/example/hibernatestarter/HibernateRunner.java
@@ -1,9 +1,24 @@
 package com.example.hibernatestarter;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class HibernateRunner {
-    public static void main(String[] args) {
-        DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",postgres,Nthvbynjh2015)
+    public static void main(String[] args) throws SQLException {
+//        Connection connection=
+//                DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","Nthvbynjh2015");
+        Configuration configuration = new Configuration();
+        configuration.configure();//объявляем конфигурационный файл по умолчанию будет искаться в resources
+
+        try(SessionFactory sessionFactory = configuration.buildSessionFactory()){//создаем factory только одну на все приложение
+            Session session = sessionFactory.openSession();
+            System.out.println("ok");//равносильно закомментированному выше коду но представляет больший рабочий функционал
+
+        }
     }
 }
