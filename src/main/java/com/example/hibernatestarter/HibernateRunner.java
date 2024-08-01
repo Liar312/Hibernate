00e65ec2a3747ff658d86303a926f99963289ce5
Liar123 <krasn20161@mail.ru>
2024-08-01 12:50:59 +0300

    полностью настроил конфиг и session factory

--- src/main/java/com/example/hibernatestarter/HibernateRunner.java
+++ src/main/java/com/example/hibernatestarter/HibernateRunner.java
@@ -1,5 +1,6 @@
 package com.example.hibernatestarter;
 
+import com.example.hibernatestarter.Models.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -7,18 +8,29 @@ import org.hibernate.cfg.Configuration;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class HibernateRunner {
+
     public static void main(String[] args) throws SQLException {
 //        Connection connection=
 //                DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","Nthvbynjh2015");
         Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(User.class);//мапим сущность
         configuration.configure();//объявляем конфигурационный файл по умолчанию будет искаться в resources
 
-        try(SessionFactory sessionFactory = configuration.buildSessionFactory()){//создаем factory только одну на все приложение
-            Session session = sessionFactory.openSession();
-            System.out.println("ok");//равносильно закомментированному выше коду но представляет больший рабочий функционал
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory()) {//создаем factory только одну на все приложение
+            Session session = sessionFactory.openSession();//равносильно закомментированному выше коду но представляет больший рабочий функционал
+            session.beginTransaction();//вручуную должны открывать и закрывать транзакции
 
+            User user = User.builder()
+                    .username("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2002, 1, 29))
+                    .age(20)
+                    .build();
+            session.save(user);
+            session.getTransaction().commit();//rollback для отката
         }
     }
 }
---
+++ src/main/java/com/example/hibernatestarter/Models/User.java
@@ -0,0 +1,32 @@
+package com.example.hibernatestarter.Models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users",schema = "public")
+public class User {
+    @Id
+    @Column(name = "username",nullable = false,unique = true)
+    private String username;
+    @Column(name="firstname")
+    private String firstname;
+    @Column(name="lastname")
+    private String lastname;
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+    @Column(name="age")
+    private Integer age;
+}
